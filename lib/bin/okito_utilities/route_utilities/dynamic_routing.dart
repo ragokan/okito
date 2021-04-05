715dcfc0e90c5e38d7c0276e4816b325904f67c3
ragokan <okanyldrmop@gmail.com>
2021-04-05 21:58:22 +0300

    rocket like routing

--- lib/bin/okito_utilities/route_utilities/dynamic_routing.dart
+++ lib/bin/okito_utilities/route_utilities/dynamic_routing.dart
@@ -62,45 +62,74 @@ Map<String, dynamic> getArgumentsFromRoute(
 // TODO: Add Document
 Route<dynamic>? pageRouteBuilder(RouteSettings settings) {
   Widget Function(BuildContext)? targetPage;
-  String? parsedString;
+  var parsedString = '';
 
   void setRoute(route, value) {
     targetPage = value;
     parsedString = route;
   }
 
+  /// We check the apps routes whether there are a route named like target
+  /// route or not.
   Okito.app.routes.forEach((route, value) =>
       (compareRoute(settings.name!, route)) ? setRoute(route, value) : null);
 
   if (targetPage != null) {
+    /// Here we give the name to show in browser
+    /// Arguments are also coming from route.
+
+    var argumentsFromRoute =
+        getArgumentsFromRoute(Uri.parse(settings.name!).path, parsedString);
+
+    /// TODO: Make this 'map'
+    final stringQuery = Uri.parse(settings.name!).hasQuery
+        ? Uri.parse(settings.name!).query
+        : null;
+
+    if (stringQuery != null) {
+      final listQuery = stringQuery.split(RegExp(r'[?&]'));
+      listQuery.forEach((element) {
+        final queryElement = element.split('=');
+        argumentsFromRoute.addAll({queryElement[0]: queryElement[1]});
+      });
+    }
+
+    settings.arguments != null
+        ? argumentsFromRoute.addAll({'arguments': settings.arguments})
+        : null;
+
+    var routeSettings =
+        RouteSettings(name: settings.name!, arguments: argumentsFromRoute);
+
+    /// We check if the app is MaterialApp or not.
+    /// If material, we show material animations, if cupertino
+    /// we show cupertino animations.
     return Okito.app.isMaterial
         ? MaterialPageRoute(
             fullscreenDialog: false,
             maintainState: true,
-            settings: RouteSettings(
-                name: settings.name,
-                arguments:
-                    getArgumentsFromRoute(settings.name ?? '', parsedString!)),
+            settings: routeSettings,
             builder: (
               context,
             ) =>
-                targetPage!(context),
-          )
+                targetPage!(context))
         : CupertinoPageRoute(
             fullscreenDialog: false,
             maintainState: true,
-            settings: RouteSettings(
-                name: settings.name,
-                arguments:
-                    getArgumentsFromRoute(settings.name ?? '', parsedString!)),
+            settings: routeSettings,
             builder: (
               context,
             ) =>
-                targetPage!(context),
-          );
+                targetPage!(context));
+  } else {
+    /// The route user wanted may not be registered to the app,
+    /// in that case we will try to push to /notFound, if that exists.
+    if (Okito.app.routes.containsKey('/notFound')) {
+      Okito.pushNamed('/notFound');
+    }
   }
 }
 
-// TODO: Add Document
+/// It just generates settings from [name] and [arguments].
 RouteSettings generateSettings({required String name, Object? arguments}) =>
     RouteSettings(name: name, arguments: arguments);
